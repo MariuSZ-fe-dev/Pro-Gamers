c1e530ff301a3075fafca443379b7c6ca926625b
Mariusz <mariusz.szyszkowski@gmail.com>
2024-10-27 12:24:46 +0100

    add hole page

---
+++ js/aos.js
@@ -0,0 +1,6 @@
+
+AOS.init({
+  startEvent: 'DOMContentLoaded',
+  offset: 100, // offset (in px) from the original trigger point
+  once: false, // whether animation should happen only once - while scrolling down
+});
